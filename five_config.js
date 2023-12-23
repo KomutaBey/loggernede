413d3834435aa37b021d7eb46d4555fa7a8371d2
KomutaBey <abdulkadirpembegul@gmail.com>
2023-12-23 22:30:47 +0300

    premium

--- five_config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-botToken:"", // The bot (normal bot) token you will use
-selfBotToken:"", // The selfbot (account bot) token you will use
-guildId:"", // The id of the server whose url you want to set
-vanityUrlCode:"" // Discord server url code you want to get
-};
